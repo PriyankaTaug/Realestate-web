7799ae3306eb78086c331fd4211d46cd3026ea27
anwarr <anwar.r@tecnots.com>
2025-12-11 14:24:17 +0530

    <>|image breaking|<anwar>

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "upload.wikimedia.org" },
-    ],
+    unoptimized: true,
   },
 };
 
